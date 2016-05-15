cc84a77bc37c332fd61bce3d43a66a6240d05ca4
Pawel <Pawel1234>
2016-05-15 23:34:24 +0200

    --help run with parameters

--- Scripts/run_bazylek.py
+++ Scripts/run_bazylek.py
@@ -1,6 +1,37 @@
-import subprocess
+from subprocess import Popen
+import sys
 
-key = 'key='+input("Set password for network : ")
 
-subprocess.Popen(['netsh', 'wlan', 'set', 'hostednetwork', 'mode=allow', 'ssid=Bazylek', key], shell=True)
-subprocess.Popen(['netsh', 'wlan', 'start', 'hostednetwork'], shell=True)
+def bazylek_help():
+    return "0 Param : python run_bazylek.py\n" \
+           "1 Param : python run_bazylek.py <password>\n" \
+           "2 Param : python run_bazylek.py <name> <password>\n" \
+           "HELP    : python run_bazylek.py --help"
+
+name = "ssid="
+key = "key="
+status = False
+
+if len(sys.argv) == 1:
+    name += input("Set network NAME     : ", )
+    key += input("Set network PASSWORD : ")
+    status = True
+elif len(sys.argv) == 2:
+    if sys.argv[1] == '--help' or sys.argv[1] == '-help':
+        print(bazylek_help())
+    elif len(sys.argv[1]) > 8:
+        name = "ssid=Bazylek"
+        key += sys.argv[1]
+        status = True
+elif len(sys.argv) == 3:
+    name += sys.argv[1]
+    key += sys.argv[2]
+    status = True
+
+if status:
+    print("Creating hotspot "+name+" !")
+    Popen(['netsh', 'wlan', 'set', 'hostednetwork', 'mode=allow', name, key], shell=True)
+    Popen(['netsh', 'wlan', 'start', 'hostednetwork'], shell=True)
+    print("")
+else:
+    print ("For help type -help or --help")
